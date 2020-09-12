8cf7256e0eaf1c564f67011d3a16885a24cac056
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-09-12 16:46:13 +0900

    Add docs

---
+++ docs/conf.py
@@ -0,0 +1,26 @@
+# Project information
+project = "demonstration"
+copyright = "2020, Akio Taniguchi"
+author = "Akio Taniguchi"
+release = "0.1.0"
+
+
+# General configuration
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
+]
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# Options for HTML output
+html_static_path = ["_static"]
+html_logo = "_static/logo.svg"
+html_theme = "pydata_sphinx_theme"
+html_theme_options = {
+    "github_url": "https://github.com/astropenguin/python-package-template/",
+    "twitter_url": "https://twitter.com/astropengu_in/",
+}
